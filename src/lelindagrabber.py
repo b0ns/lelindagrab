c55384b03dbda07051a38e5501fdbd2a5e9ee34c
b0ns <ivan.bonacci@base-camp.cc>
2012-01-02 17:11:11 +0100

    Added custom date argument input

--- src/lelindagrabber.py
+++ src/lelindagrabber.py
@@ -2,23 +2,25 @@
 Created on Jan 2, 2012
 
 @author: bons
+
 '''
 
-
 #!/usr/bin/python
+
+import sys
 import urllib
 import time
 from datetime import datetime,timedelta
 
 
+d_format = "%Y/%m/%d"
 
 def retrieveImages( period,hour, path):
     
     thisdate = time.strftime("%Y/%m/%d", period.timetuple())
     url = "http://www.webcam-ski.com/archives/" + thisdate +"/"+ hour + "/lindarets/lindarets_MEGA.jpg"
-    print url
     name = time.strftime("%Y_%m_%d", period.timetuple())+hour
-    print name
+    print "Got pic: " + name
     urllib.urlretrieve( url, path+name)
   
 
@@ -27,14 +29,22 @@ def daterange(start_date, end_date):
         yield start_date + timedelta(n)
 
 if __name__ == '__main__':
-    h_list = ["1000","1400","1600"]
-    start_date = datetime(2011,12,01)
-    end_date = datetime(2011,12,31)
+    try:
+        start_date = datetime.strptime(sys.argv[1], d_format)
+        end_date = datetime.strptime(sys.argv[2], d_format) 
+    except (TypeError, IndexError):
+        sys.stderr.write("Example: lelindagrabber.py 2011/03/25 2011/04/02 \n")
+        
+    print "Le LindaGrabber Started \n"
+    print "retrieving pictures from" + str(start_date) +" to " + str(end_date)
+       
+    h_list = ["1000","1400","1600"] #daily pic hours list
     path = "./pics/" #The path where you want to store the downloaded images
     day_count = (end_date - start_date).days + 1
     for date in daterange(start_date, end_date):
         for hour in h_list:
             retrieveImages( date, hour, path )
+     
      
 
 

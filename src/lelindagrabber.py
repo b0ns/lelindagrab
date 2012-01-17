025058a2f0d035f4d6dad8b52b0dc692ed27f54a
b0ns <ivan.bonacci@base-camp.cc>
2012-01-17 20:27:48 +0100

    little date change

--- src/lelindagrabber.py
+++ src/lelindagrabber.py
@@ -30,11 +30,17 @@ def daterange(start_date, end_date):
         yield start_date + timedelta(n)
 
 if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print 'Error: no date argument specified'
+        print 'Usage: lelindagrabber.py 2011/12/20 2011/12/23 \n'
+        sys.exit(1)
     try:
         start_date = datetime.strptime(sys.argv[1], d_format)
         end_date = datetime.strptime(sys.argv[2], d_format) 
     except (TypeError, IndexError):
         sys.stderr.write("Example: lelindagrabber.py 2011/12/20 2011/12/23 \n")
+        sys.exit(1)
+
         
     print "Le LindaGrabber Started \n"
     print "retrieving pictures from" + str(start_date) +" to " + str(end_date)
